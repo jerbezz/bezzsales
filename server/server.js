58c65dbca3780c79e08769b20b610b48316c4b70
Jeremy Bezzant <jerbezz@gmail.com>
2019-09-16 14:48:42 -0600

    back end set up

---
+++ server/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+
+//controllers from server/controllers
+
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+
+const app = express()
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+    app.listen(SERVER_PORT, () => {
+        console.log(`Still got it on port: ${SERVER_PORT}`)
+    })
+    console.log(db.listTables())
+})
+
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+// endpoints for controllers
