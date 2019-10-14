e85062d4e6e087306e9f93c6b7166feb323647ef
Jeremy Bezzant <jerbezz@gmail.com>
2019-10-14 12:32:17 -0600

    seed file fix

--- db/seed.sql
+++ db/seed.sql
@@ -1,51 +1,51 @@
 create table company (
-    companyId serial,
+    companyId serial primary key,
     companyName varchar(255)
-)
+);
 
-create table user (
-    userId serial,
+create table users (
+    userId serial primary key,
     isAdmin boolean,
     email text,
     passHash text,
     firstName varchar(50),
     lastname varchar(50),
-    companyId int references company(companyId)
-)
+    companyId integer references company(companyId)
+);
 
 create table stores (
-    storeId serial,
-    companyId int references company(companyId),
+    storeId serial primary key,
+    companyId integer references company(companyId),
     storeName varchar(100)
-)
+);
 
 create table salesItems (
-    itemNumber serial,
-    storeId int references stores(storeId),
+    itemNumber serial primary key,
+    storeId integer references stores(storeId),
     itemName varchar(50),
     itemDesc varchar(50),
-    itemPrice int
-)
+    itemPrice integer
+);
 
 create table customers (
-    customerNumber serial,
-    storeId int references stores(storeId),
-    custName text(75),
+    customerNumber serial primary key,
+    storeId integer references stores(storeId),
+    custName varchar(75),
     custAddress text,
     custState varchar(2),
-    custZip int(5)
-)
+    custZip numeric(5)
+);
 
 create table purchases (
-    purchaseNumber serial,
-    storeId int references stores(storeId),
+    purchaseNumber serial primary key,
+    storeId integer references stores(storeId),
     vendor varchar(55),
-    amount int,
+    amount integer,
     itemDesc varchar(55)
-)
+);
 
 create table sales (
-    salesNumber serial,
-    storeId int references stores(storeId),
-    itemNumber int references salesItems(itemNubmer)
-)
+    salesNumber serial primary key,
+    storeId integer references stores(storeId),
+    itemNumber integer references salesItems(itemNumber)
+);
