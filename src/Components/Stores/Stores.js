408ff5718df7078b0320c8696673fff6d3d77fc1
Jeremy Bezzant <jerbezz@gmail.com>
2019-10-22 15:55:52 -0600

    create company set up

--- src/Components/Stores/Stores.js
+++ src/Components/Stores/Stores.js
@@ -1,9 +1,10 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-function Stores () {
+function Stores() {
     const [data, setData] = useState([])
+    const [company, setCompany] = useState('')
 
     async function getUserData() {
         const response = await axios.get('/auth/user-data')
@@ -13,14 +14,32 @@ function Stores () {
         getUserData()
     }, [])
 
-    console.log({data})
-    return (
-    <div>
+    const handleCreateCompany = async (e) => {
+        e.preventDefault()
+        await axios.post("/company/create", {
+            companyName: company
+        })
+    }
 
-    <h1>List of Stores</h1>
-    <Link to='/sorp'><button>Store 1</button></Link>
-    </div>
-    )
+    console.log({ data })
+    return data.companyId ? (
+        <div>
+
+            <h1>List of Stores</h1>
+            <Link to='/sorp'><button>Store 1</button></Link>
+        </div>
+    ) :
+        (
+            <div>Create A Company to Get Started
+            <form onSubmit={handleCreateCompany}>
+                    <input type='text' required
+                        value={company}
+                        onChange={(e) => setCompany(e.target.value)}
+                        placeholder='Company Name' />
+                    <input type='submit' value='Create Company' />
+                </form>
+            </div>
+        )
 }
 
 export default Stores
